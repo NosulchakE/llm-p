3dacbba1f1c80f10a7c81a96c787e54bfe56ed38
Elena Nosulchak <133764080+NosulchakE@users.noreply.github.com>
2026-04-28 12:48:26 +0300

    Create user.py

---
+++ app/schemas/user.py
@@ -0,0 +1,10 @@
+# app/schemas/user.py
+from pydantic import BaseModel, ConfigDict
+
+
+class UserPublic(BaseModel):
+    id: int
+    email: str
+    role: str
+    
+    model_config = ConfigDict(from_attributes=True)
